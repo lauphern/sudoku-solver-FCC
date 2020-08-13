267fad3f30bbfa3b268a950c7aa5c70142b5db86
lauphern <lau.p.hern@hotmail.com>
2020-08-13 20:47:18 +0200

    added validation in method paintBoard so the user enters only correct values

--- public/sudoku-solver.js
+++ public/sudoku-solver.js
@@ -1,32 +1,41 @@
-const textArea = document.getElementById('text-input');
-import { puzzlesAndSolutions } from './puzzle-strings.js';
+const textArea = document.getElementById("text-input");
+import { puzzlesAndSolutions } from "./puzzle-strings.js";
 
 class Sudoku {
   constructor() {
     this.inputArray = document.querySelectorAll("table.grid input");
   }
-  paintBoard = (str) => {
-    if(this.inputArray.length !== str.length) {
-      document.querySelector("#error-msg").innerText = "Error: Expected puzzle to be 81 characters long.";
+  paintBoard = str => {
+    if (this.inputArray.length !== str.length) {
+      document.querySelector("#error-msg").innerText =
+        "Error: Expected puzzle to be 81 characters long.";
     } else {
       document.querySelector("#error-msg").innerText = "";
-      for(let i = 0; i < str.length; i++) {
+      for (let i = 0; i < str.length; i++) {
+        if (!/([1-9]|\.)+/gm.test(str[i])) {
+          document.querySelector("#error-msg").innerText =
+            "Please enter only numbers in the range of 1-9 and dots (.)";
+          break;
+        }
         this.inputArray[i].value = str[i] === "." ? "" : str[i];
       }
     }
-  }
+  };
 }
 
+const sudoku = new Sudoku();
 
-const sudoku = new Sudoku()
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // Load a simple puzzle into the text area
-  textArea.value = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-  sudoku.paintBoard(textArea.value)
+  textArea.value =
+    "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
+  sudoku.paintBoard(textArea.value);
 });
 
-textArea.addEventListener("input", function() {console.log("changing");sudoku.paintBoard(this.value)});
+textArea.addEventListener("input", function () {
+  console.log("changing");
+  sudoku.paintBoard(this.value);
+});
 
 /* 
   Export your functions for testing in Node.
@@ -35,6 +44,6 @@ textArea.addEventListener("input", function() {console.log("changing");sudoku.pa
 */
 try {
   module.exports = {
-    Sudoku
-  }
+    Sudoku,
+  };
 } catch (e) {}
